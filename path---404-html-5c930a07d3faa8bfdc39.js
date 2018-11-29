8ea98d1d0c36e2ed370c47c7fd0cd4be98c87b05
melophonic <melophonic@gmail.com>
2018-11-29 12:26:22 -0800

    Updates

---
+++ path---404-html-5c930a07d3faa8bfdc39.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xa2868bfb69fc],{307:function(t,o){t.exports={data:{site:{siteMetadata:{title:"Django Meets Blockchain",description:"Documentation for Django Meets Blockchain",siteUrl:"https://taliotech.github.io",keywords:"talio,blockchain,django",author:{name:"Talio.io",url:"https://talio.ai",email:"info@talio.io"}}}},pathContext:{}}}});
+//# sourceMappingURL=path---404-html-5c930a07d3faa8bfdc39.js.map
